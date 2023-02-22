2752db30e1f13e983d39c61426e46a39972b59c7
milanjrodd <milanrodd@gmail.com>
2023-02-22 18:16:33 +0400

    init typography

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -18,6 +18,20 @@ body {
   background-color: ${({ theme }) => theme.colors.black};
 }
 
+//========================================================================================================
+// TYPOGRAPHY
+//========================================================================================================
+h4 {
+	font-family: ${({ theme }) => theme.fontFamily.cinzel};
+	font-style: normal;
+	font-weight: ${({ theme }) => theme.fontWeight.medium};
+	font-size: ${({ theme }) => theme.fontSize['4xs']};
+	line-height: 19px;
+	
+	margin: ${({ theme }) => theme.spacing['6xs']} 0;
+}
+
+
 `;
 
 export default GlobalStyle;
