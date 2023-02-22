062b48b58a8c3d4c83bfb0bdbd5d49323b22b4e0
milanjrodd <milanrodd@gmail.com>
2023-02-22 18:19:22 +0400

    init home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
 import Head from 'next/head';
+import axios from 'axios';
+import { QuestCardList } from '@/components/quest-card-list';
 
 export default function Home() {
+	const fetchQuests = async () => {
+		try {
+			const res = await axios.get<IQuestResponse[]>('/api/quests');
+
+			// Remap data response to the IQuest interface
+			const quests: IQuest[] = res.data.map((quest) => ({
+				id: quest.id,
+				title: quest.title,
+				cover: quest.cover,
+				params: {
+					skillTree: quest.skillTree,
+					skill: quest.skill,
+					difficulty: quest.difficulty,
+					experience: quest.experience,
+					gold: quest.gold,
+					type: quest.type
+				}
+			}));
+
+			return quests;
+		} catch (error) {
+			throw new Error('Failed to fetch quests');
+		}
+	};
+
+	const questsQuery = useQuery({ queryKey: ['quests'], queryFn: fetchQuests });
+
 	return (
 		<>
 			<Head>
@@ -8,7 +38,14 @@ export default function Home() {
 				<meta name='description' content='Node Guardians frontend' />
 			</Head>
 
-			<main></main>
+			<main>
+				<div>
+					<h1>Node Guardians</h1>
+				</div>
+				<div>
+					<QuestCardList quests={questsQuery.data} />
+				</div>
+			</main>
 		</>
 	);
 }
