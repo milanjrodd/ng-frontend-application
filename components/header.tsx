a950c8b6272dea1b9250099b6ec103592baa9127
milanjrodd <milanrodd@gmail.com>
2023-02-23 20:17:48 +0400

    fix header content alignment

--- components/header.tsx
+++ components/header.tsx
@@ -10,16 +10,15 @@ const StyledHeaderContent = styled.div`
 	width: 100%;
 	max-width: 1400px;
 	padding: ${({ theme }) => theme.spacing['3.5xs']} ${({ theme }) => theme.spacing['2s']};
+	margin: 0 auto;
 `;
 
 export const Header: React.FC<{}> = ({}) => {
 	return (
-		<>
-			<StyledHeader>
-				<StyledHeaderContent>
-					<h1>Node Guardians</h1>
-				</StyledHeaderContent>
-			</StyledHeader>
-		</>
+		<StyledHeader>
+			<StyledHeaderContent>
+				<h1>Node Guardians</h1>
+			</StyledHeaderContent>
+		</StyledHeader>
 	);
 };
